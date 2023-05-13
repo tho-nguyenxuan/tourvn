89b2c1ba42180df6d413b0ac4da048ac74272251
unknown <tho.nguyenxuantho573@hcmut.edu.vn>
2023-05-13 09:44:21 +0700

    Add simple content for homepage

--- client/src/Pages/HomePage/HomePage.js
+++ client/src/Pages/HomePage/HomePage.js
@@ -1,5 +1,16 @@
 function HomePage() {
-	return <div className="App" style={{ height: "2000px" }}></div>;
+	return (
+		<div
+			style={{
+				marginTop: "100px",
+				display: "flex",
+				justifyContent: "center",
+				height: "2000px",
+			}}
+		>
+			<h1>Home Page</h1>
+		</div>
+	);
 }
 
 export default HomePage;
