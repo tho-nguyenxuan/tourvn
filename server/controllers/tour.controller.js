254a945657c20a5ea77b381b17787d474dfbdb6f
unknown <tho.nguyenxuantho573@hcmut.edu.vn>
2023-06-09 23:04:56 +0700

    commit

--- server/controllers/tour.controller.js
+++ server/controllers/tour.controller.js
@@ -1,5 +1,6 @@
 import { createReadStream } from "fs";
 import csvParser from "csv-parser";
+import db from "../db.js";
 
 const result = [];
 
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,3 +1,22 @@
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+	host: HOST,
+	user: USERNAME,
+	password: PASSWORD,
+	database: DATABASE,
+	port: PORT,
+});
+
+con.connect(function (err) {
+	if (err) throw err;
+	console.log("Connected!");
+});
+
+con.query("SELECT 1+1").on("result", function (row) {
+	console.log(row);
+});
+
 import { createHash } from "crypto";
 
 const password = "password";
