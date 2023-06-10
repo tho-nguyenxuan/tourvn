226d1bb96433e89392bd955cd367ecfd79a1686c
unknown <tho.nguyenxuantho573@hcmut.edu.vn>
2023-06-10 11:39:25 +0700

    commit

--- server/controllers/tour.controller.js
+++ server/controllers/tour.controller.js
@@ -1,36 +1,37 @@
-import { createReadStream } from "fs";
-import csvParser from "csv-parser";
-import db from "../db.js";
-
-const result = [];
-
-createReadStream("tours.csv")
-	.pipe(csvParser({ headers: false, skipLines: 1 }))
-	.on("data", (data) => {
-		// console.log(data);
-		// result.push(Object.values(data));
-		result.push({
-			source: data[0],
-			destination: data[1],
-			start: data[2],
-			end: data[3],
-			time: data[4],
-			title: data[5],
-			imgUrl: data[6],
-			description: data[7],
-			program: data[8],
-			price: data[9],
-			childrenPrice: data[10],
-			linkUrl: data[11],
-		});
-	})
-	.on("end", () => {
-		// console.log(result);
-	});
+import db, { result } from "../db.js";
 
 const getTour = async (req, res) => {
-	res.json(result);
-	res.end();
+	var temp = [];
+	db.query("CALL DSTour()", function (error, results, fields) {
+		if (error) console.log(error);
+		for (const data of results[0]) {
+			temp.push({
+				// source: data[0],
+				// destination: data[1],
+				id: data.Ma,
+				start: data.ThoiGianDi.toISOString()
+					.substring(0, 10)
+					.split("-")
+					.reverse()
+					.join("/"),
+				// end: data[3],
+				time: data.ThoiLuong,
+				title: data.TieuDe,
+				imgUrl: data.Anh,
+				description: data.MoTa,
+				// program: data[8],
+				price: data.GiaNguoiLon,
+				// childrenPrice: data[10],
+				linkUrl: data.DuongLink,
+			});
+		}
+		// res.send(results[0]);
+		// console.log(results[0][0].ThoiGianDi.toISOString().substring(0, 10));
+		res.status(200).json(temp);
+		// console.log(temp);
+	});
+	// res.json(result);
+	// res.end();
 };
 
 export { getTour };
